769297b76e2d68e262e1b06e9f1bce686abac9f1
Rob Mayo <rbrtm984@gmail.com>
2023-09-20 15:06:04 -0400

    made progress setting up express server, querying wakatime api, setting up react components

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,5 @@
-import _ from 'lodash';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+ReactDOM.render(<App />, document.getElementById('root'));
