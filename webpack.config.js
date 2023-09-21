0a24b92633acd9827d1b6383ffd553dd00382e4a
Rob Mayo <rbrtm984@gmail.com>
2023-09-20 22:28:40 -0400

    commiting all work on solo project aside from wakatime controller which contains sensitive info app currently  queries wakatime api and displays hours and minutes spent working on specified project

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    // Starting point of the app, where webpack begins its bundling
     entry: './client/src/index.js',
+    // Mode set to development (can be 'production' for optimized build)
     mode: 'development',
     module: {
         rules: [
+          // Transpile JSX (React) files into vanilla JS
             {
                 test: /\.jsx?/ ,
                 exclude: /node_modules/,
@@ -21,23 +24,29 @@ module.exports = {
               }
         ]
     },
+    // Resolve .js and .jsx file extensions when importing
     resolve: {
         extensions: ['.js', '.jsx']
     },
     devServer: {
         host: 'localhost',
         port: 8080,
+        // Serve files from the specified directory
         static: {
             directory: path.resolve(__dirname, 'client/dist'),
             publicPath: '/'
         },
+        // Hot-reloading: auto-refresh browser on changes
         hot: true,
+        // Fallback to index.html for Single Page Applications
         historyApiFallback: true,
         headers: {'Access-Control-Allow-Origin': '*'},
+        // Proxy API requests to our express server running on port 3000
         proxy: {
-            '/api/**': {
+            '/api': {
               target: 'http://localhost:3000/',
               secure: false,
+              changeOrigin: true
             },
             '/assets/**': {
               target: 'http://localhost:3000/',
@@ -45,11 +54,15 @@ module.exports = {
             },
           },
     },
+    // Where the bundled output will be saved
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [new HtmlWebpackPlugin({
+    // Plugins to extend webpack's functionality
+    plugins: [
+      // Auto-generate an HTML file that includes the webpack bundles
+      new HtmlWebpackPlugin({
         title: 'Development',
         template: path.resolve(__dirname, 'client', 'src', 'index.html')
     })],
