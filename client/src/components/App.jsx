dff25559bbf33eb0ca68a745b334b1210aaa01d6
Rob Mayo <rbrtm984@gmail.com>
2023-12-09 13:49:51 -0500

    added form component and adjusted from default

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Hero from './Hero';
+import Form from './Form';
 import '../styles/global.css'
 
 const App = () => {
   return (
   <div id="mainContainer" className="bg-gray-900">
     <Hero/>
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <Form/>
+    </div>
+    
   </div>
   );
 };
