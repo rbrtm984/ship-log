d68f30d96d8706781d6c6fc22755ba5046e37ab9
Rob Mayo <rbrtm984@gmail.com>
2023-12-10 18:13:14 -0500

    added bar chart component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Hero from './Hero';
 import Form from './Form';
+import BarChart from './Bar';
 import '../styles/global.css'
 
 const App = () => {
+  const [chartData, setChartData] = useState([]);
+  const handleFormData = (data) => {
+    setChartData(prevData => [...prevData, data]);
+  }
+
   return (
   <div id="mainContainer" className="bg-gray-900">
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <Hero/>
-      <Form/>
+      <Form onFormData={handleFormData} />
+      <BarChart chartData={chartData}/>
     </div>
   </div>
   );
---
+++ client/src/components/Bar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+
+
+const BarChart = ({ chartData }) => {
+    chartData = [
+        { label: 'Monday', value: 8 },
+        { label: 'Tuesday', value: 6 },
+        { label: 'Wednesday', value: 7 },
+        { label: 'Thursday', value: 9 },
+        { label: 'Friday', value: 5 },
+        { label: 'Saturday', value: 10 },
+        { label: 'Sunday', value: 8 },
+        ];
+  // Define the chart's data and configuration
+  const data = {
+    labels: chartData.map(d => d.label),
+    datasets: [
+      {
+        label: 'Hours Worked',
+        data: chartData.map(d => d.value),
+        backgroundColor: 'rgba(54, 162, 235, 0.5)',
+      },
+    ],
+};
+
+  return <Bar data={data} />;
+};
+
+export default BarChart;
