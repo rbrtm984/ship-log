dfbd4d82997badf39b387af0ca61fa88ae800efa
Rob Mayo <rbrtm984@gmail.com>
2023-12-31 12:05:21 -0500

    removed old tests and decided to restart the whole project

--- __tests__/test_Hero.jsx
+++ __tests__/test_Hero.jsx
@@ -1,64 +1,34 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import { toBeInTheDocument, getAllByRole } from '@testing-library/jest-dom'; // Add the import statement for toBeInTheDocument
+import { render, screen } from '@testing-library/react';
 import Hero from '../client/src/components/Hero';
+// import { toBeInTheDocument } from '@testing-library/jest-dom/extend-expect';
+// import '@testing-library/jest-dom/extend-expect';
 
-describe('Hero', () => {
-    // Import the extend-expect function
+describe('Hero component', () => {
+  test('renders logo image', () => {
+    render(<Hero />);
+    const logoImages = screen.queryAllByAltText('ship log logo');
+    expect(logoImages).toHaveLength(2);
+  });
 
-    test('renders navigation correctly', () => {
-        const { getByText } = render(<Hero />);
-        // Assert that navigation items are rendered correctly
-        expect(getByText('GitHub')).toBeInTheDocument(); // Use toBeInTheDocument function
-        expect(getByText('Team')).toBeInTheDocument(); // Use toBeInTheDocument function
-        expect(getByText('Account')).toBeInTheDocument(); // Use toBeInTheDocument function
-    });
+  test('renders navigation links', () => {
+    render(<Hero />);
+    const navigationLinks = screen.getAllByRole('link');
+    expect(navigationLinks).toHaveLength(5);
+  });
 
-    test('toggles menu on button click', () => {
-        const { getByRole, getByText } = render(<Hero />);
+  test('toggles menu state on button click', () => {
+    render(<Hero />);
+    const menuButtons = screen.queryAllByLabelText('Toggle Menu');
+    expect(menuButtons).toHaveLength(2);
 
+    // Menu state should be null initially
+    expect(screen.queryByText('GitHub')).not.toBeInTheDocument();
 
-        const menuButtons = getAllByRole('button', { name: /toggle menu/i });
-        const menuButton = menuButtons[0]; // Select the desired menu button
+    // Click the menu button to toggle the menu state
+    menuButtons[0].click();
 
-        // Assert that menu is initially hidden
-        expect(getByText('GitHub')).not.toBeVisible();
-        expect(getByText('Team')).not.toBeVisible();
-        expect(getByText('Account')).not.toBeVisible();
-
-        // Click the menu button to toggle the menu
-        fireEvent.click(menuButton);
-
-        // Assert that menu is now visible
-        expect(getByText('GitHub')).toBeVisible();
-        expect(getByText('Team')).toBeVisible();
-        expect(getByText('Account')).toBeVisible();
-
-        // Click the menu button again to hide the menu
-        fireEvent.click(menuButton);
-
-        // Assert that menu is hidden again
-        expect(getByText('GitHub')).not.toBeVisible();
-        expect(getByText('Team')).not.toBeVisible();
-        expect(getByText('Account')).not.toBeVisible();
-    });
-
-    test('renders brand logo correctly', () => {
-        const { getByAltText } = render(<Hero />);
-
-        // Assert that brand logo is rendered correctly
-        expect(getByAltText('ship log logo')).toBeInTheDocument();
-    });
-
-    test('renders section content correctly', () => {
-        const { getByText } = render(<Hero />);
-
-        // Assert that section content is rendered correctly
-        expect(getByText('Ship Log:')).toBeInTheDocument();
-        expect(
-            getByText(
-                '(noun) an official record of events during a particular period of time, especially a journey on a ship or plane'
-            )
-        ).toBeInTheDocument();
-    });
+    // Menu state should be true after clicking the button
+    expect(screen.queryByText('GitHub')).toBeInTheDocument();
+  });
 });
--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     },
     moduleNameMapper: {
       '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-    }
+    },
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
   };
   
